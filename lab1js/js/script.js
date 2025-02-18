8ceb61b27a0970d1f926cfc26b2bbf7c59ca0548
ShakuraVlad <svladiusha@bk.ru>
2025-02-18 12:14:22 +0200

    Add files via upload

---
+++ lab1js/js/script.js
@@ -0,0 +1,22 @@
+alert("Этот код выполнен из внешнего файла!");
+   console.log("Сообщение в консоли");
+
+   let birthYear = 20;
+   const Name = 'Владислав';
+   var isStudent = 25;
+   console.log(birthYear, Name, isStudent);
+
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+  console.log("Отлично!");
+} else if (score >= 70) {
+  console.log("Хорошо");
+} else {
+  console.log("Можно лучше!");
+}
+
+for (let i = 1; i <= 5; i++) {
+  console.log(`Итерация: ${i}`);
+}
+
+
